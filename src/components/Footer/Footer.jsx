3a493261f97f2157203dad6b9b7da397473eaae9
tonistorres <sisvenda2011@gmail.com>
2022-06-18 11:38:57 -0300

    fix: atualizando melhorias componente fotter

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,12 +6,24 @@ import './Footer.css';
 // https://www.youtube.com/watch?v=5VFlZpgfu48&list=PLR8OzKI52ppXaPGdUUQGOGKeXSGbChIbc&index=4
 function Footer() {
     return (
-        <div className='container col-12 pt-3 text-light  footer text-center  bg-secondary '>
-            <div className='container col-6 col-md-6 d-flex justify-content-around '>
-                <FaFacebook size={28} />
-                <FaInstagram size={28} />
-                <GoMarkGithub size={28} />
-                <span className="text-monospace">
+        <div className='container col-12 pt-3 text-light  footer text-center  bg-secondary rounded'>
+            <div className='d-flex  justify-content-around align-items-center'>
+                <span>
+                    <a href="https://www.facebook.com/datorres.ferreira/">
+                        <FaFacebook size={28} />
+                    </a>
+                </span>
+                <span>
+                    <a href="https://www.instagram.com/tonistorresferreira/">
+                        <FaInstagram size={28} />
+                    </a>
+                </span>
+                <span>
+                    <a href="https://github.com/tonistorres">
+                        <GoMarkGithub size={28} />
+                    </a>
+                </span>
+                <span>
                     <a href="https://www.linkedin.com/in/tonistorres/">
                         <FaLinkedin size={28} />
                     </a>
