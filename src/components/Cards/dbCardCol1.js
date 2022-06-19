1324340acffb4ae414491a520b9d5610af5f028f
tonistorres <sisvenda2011@gmail.com>
2022-06-19 15:06:41 -0300

    feat: adicionando projeto Portfólio versao beta ao projeto

--- src/components/Cards/dbCardCol1.js
+++ src/components/Cards/dbCardCol1.js
@@ -1,14 +1,14 @@
 import ImgTrybeTunes from "../../assets/img/trybeTunes.png";
 import ImgAppReserve from '../../assets/img/fullStack.gif';
 
- const dbColuna1 =[
- {
-    project:'Trybe Tunes',
+const dbColuna1 = [
+  {
+    project: 'Trybe Tunes',
     img: ImgTrybeTunes,
-    linkLikedin:'https://www.linkedin.com/posts/tonistorres_dei-um-tapa-no-visual-rsrs-pouquito-de-styled-activity-6881661367984685056-72yM?utm_source=linkedin_share&utm_medium=member_desktop_web',
-    linkYoutube:'https://www.youtube.com/embed/IETKR8cXaHg',
-    gitHub:'https://github.com/tonistorres/project-trybe-tunes',
-    arrHabilidades:[
+    linkLikedin: 'https://www.linkedin.com/posts/tonistorres_dei-um-tapa-no-visual-rsrs-pouquito-de-styled-activity-6881661367984685056-72yM?utm_source=linkedin_share&utm_medium=member_desktop_web',
+    linkYoutube: 'https://www.youtube.com/embed/IETKR8cXaHg',
+    gitHub: 'https://github.com/tonistorres/project-trybe-tunes',
+    arrHabilidades: [
       'Executar uma requisição e consumir dados vindos de uma API;',
       'Utilizar os ciclos de vida de um componente React;',
       'Utilizar a função setState de forma a garantir que um determinado código só é executado após o estado ser atualizado;',
@@ -18,14 +18,14 @@ import ImgAppReserve from '../../assets/img/fullStack.gif';
       'Usar o componente Redirect pra redirecionar para uma rota específica;',
       'Criar links de navegação na aplicação com o componente Link;'
     ]
-   },
-   {
-    project:'App Reserva',
+  },
+  {
+    project: 'App Reserva',
     img: ImgAppReserve,
-    linkLikedin:'https://www.linkedin.com/posts/tonistorres_projeto-crud-back-e-front-bibliotecas-e-activity-6930562357395894273-3Yfw?utm_source=linkedin_share&utm_medium=member_desktop_web',
+    linkLikedin: 'https://www.linkedin.com/posts/tonistorres_projeto-crud-back-e-front-bibliotecas-e-activity-6930562357395894273-3Yfw?utm_source=linkedin_share&utm_medium=member_desktop_web',
     linkYoutube: 'https://www.youtube.com/embed/o94_-a2rw14',
-    gitHub:'https://github.com/tonistorres/crud-back-front',
-    arrHabilidades:[
+    gitHub: 'https://github.com/tonistorres/crud-back-front',
+    arrHabilidades: [
       'Executar uma requisição e consumir dados vindos de uma API;',
       'Utilizar os ciclos de vida de um componente React;',
       'Utilizar Hooks;',
@@ -42,7 +42,32 @@ import ImgAppReserve from '../../assets/img/fullStack.gif';
       'Tercerizando a camada Model com Sequelize',
       'Utilizando token JWT'
     ]
-   }
+  },
+  {
+    project: 'Portfólio Responsivo Beta',
+    img: ImgAppReserve,
+    linkLikedin: 'https://www.linkedin.com/posts/tonistorres_depoisde-umasemana-focadono-meuportf%C3%B3lio-activity-6944094371713589248-kjZq?utm_source=linkedin_share&utm_medium=member_desktop_web',
+    linkYoutube: 'https://www.youtube.com/embed/Xzv7ZslfztI',
+    gitHub: 'https://github.com/tonistorres/portifolio-bootstrap-heroku',
+    arrHabilidades: [
+      'Executar uma requisição e consumir dados vindos de uma API;',
+      'Utilizar os ciclos de vida de um componente React;',
+      'Utilizar Hooks;',
+      'Utilizar Context;',
+      'Utilizar Provider;',
+      'Utilizar Props;',
+      'Utilizar o componente BrowserRouter corretamente;',
+      'Utilizar o Switch do React Router;',
+      'Usar o componente Redirect pra redirecionar para uma rota específica;',
+      'Criar links de navegação na aplicação com o componente Link;',
+      'Indexar links do Youtube ao App;',
+      'Conceitos de grid com bootstrap;',
+      'Conceitos de responsividade com bootstrap;',
+      'Classe container com bootstrap;',
+      'Flex-box no bootstrap;',
+      'Conceitos de media query com bootstrap;'
+    ]
+  },
 
- ]
+]
 export default dbColuna1;
